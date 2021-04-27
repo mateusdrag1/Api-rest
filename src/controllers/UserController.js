c2f053b534054db7ecb8be6981df795a0b3b3e6e
Matthieu Christian <matthieu.christian.098@ufrn.edu.br>
2021-04-27 03:49:33 -0300

    CRUD de Usuário 🧑

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,9 +4,71 @@ class UserController {
   async store(req, res) {
     try {
       const novoUser = await User.create(req.body);
-      res.json(novoUser);
+      return res.json(novoUser);
     } catch (err) {
-      res.status(400).json({ erros: err.errors.map((error) => error.message) });
+      return res.status(400).json({ erros: err.errors.map((error) => error.message) });
+    }
+  }
+
+  // Index
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (err) {
+      return res.status(400).json({ erros: err.errors.map((error) => error.message) });
+    }
+  }
+
+  // Show
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      return res.json(user);
+    } catch (err) {
+      return res.status(400).json({ erros: err.errors.map((error) => error.message) });
+    }
+  }
+
+  // Update
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['ID não enviado'],
+        });
+      }
+      const user = await User.findByPk(req.params.id);
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe'],
+        });
+      }
+      const novosDados = await user.update(req.body);
+      return res.json(novosDados);
+    } catch (err) {
+      return res.status(400).json({ erros: err.errors.map((error) => error.message) });
+    }
+  }
+
+  // Delete
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['ID não enviado'],
+        });
+      }
+      const user = await User.findByPk(req.params.id);
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe'],
+        });
+      }
+      await user.destroy();
+      return res.status(200).json({ msg: 'Usuário apagado com sucesso!' });
+    } catch (err) {
+      return res.status(400).json({ erros: err.errors.map((error) => error.message) });
     }
   }
 }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,5 +4,9 @@ import userController from '../controllers/UserController';
 const router = new Router();
 
 router.post('/', userController.store);
+router.get('/', userController.index);
+router.get('/:id', userController.show);
+router.put('/:id', userController.update);
+router.delete('/:id', userController.delete);
 
 export default router;
