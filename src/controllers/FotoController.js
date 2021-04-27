4043efc6b25a9434325e964b31b6d2ca5f806829
Matthieu Christian <matthieu.christian.098@ufrn.edu.br>
2021-04-27 06:06:33 -0300

    Tratamento dos arquivos de imagem 🤳

--- src/controllers/FotoController.js
+++ src/controllers/FotoController.js
@@ -1,6 +1,20 @@
+import multer from 'multer';
+
+import multerConfig from '../config/multer';
+
+const upload = multer(multerConfig).single('foto');
+
 class FotoController {
   async store(req, res) {
-    res.json(req.file);
+    return upload(req, res, (err) => {
+      if (err) {
+        return res.status(400).json({
+          errors: [err.code],
+        });
+      }
+
+      return res.json(req.file);
+    });
   }
 }
 
