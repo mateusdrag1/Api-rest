f43ea6ee475ce700d6a8b40c3cdc342962ef0ab2
Matthieu Christian <matthieu.christian.098@ufrn.edu.br>
2021-04-27 04:24:35 -0300

    Middleware para Autenticação JWT 🆔

---
+++ src/middlewares/loginRequired.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken';
+
+export default (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json({
+      errors: ['Está faltando um TOKEN'],
+    });
+  }
+
+  const [, token] = authorization.split(' ');
+
+  try {
+    const dados = jwt.verify(token, process.env.TOKEN_SECRET);
+    const { id, email } = dados;
+    req.userId = id;
+    req.userEmail = email;
+    return next;
+  } catch (err) {
+    return res.status(401).json({
+      errors: ['Token expirado ou inválido'],
+    });
+  }
+};
